2d258fabcbc33c1cbda3fedfc4f7fcb39d3f4971
anh.vu <acton@enclave.vn>
2017-08-14 10:04:54 +0700

    Edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,16 @@ deploy(){
   echo "Stating Deploy NghiaVuQuanSu System"
   git pull
   mvn clean
-  mvn package
+  mvn tomcat7:undeploy
+  mvn tomcat7:redeploy
 
-  sh /home/ec2-user/tomcat/apache-tomcat-7.0.79/bin/shutdown.sh
-  rm -rf /home/ec2-user/tomcat/apache-tomcat-7.0.79/webapps/ROOT
-  mv /home/ec2-user/NVQS/target/nghiavuquansu-0.0.1-SNAPSHOT.war /home/ec2-user/tomcat/apache-tomcat-7.0.79/webapps/ROOT.war
-  sh /home/ec2-user/tomcat/apache-tomcat-7.0.79/bin/startup.sh
-  tail -f /home/ec2-user/tomcat/apache-tomcat-7.0.79/logs/catalina.out
+  # mvn package
+
+  # sh /home/ec2-user/tomcat/apache-tomcat-7.0.79/bin/shutdown.sh
+  # rm -rf /home/ec2-user/tomcat/apache-tomcat-7.0.79/webapps/ROOT
+  # mv /home/ec2-user/NVQS/target/nghiavuquansu-0.0.1-SNAPSHOT.war /home/ec2-user/tomcat/apache-tomcat-7.0.79/webapps/ROOT.war
+  # sh /home/ec2-user/tomcat/apache-tomcat-7.0.79/bin/startup.sh
+  # tail -f /home/ec2-user/tomcat/apache-tomcat-7.0.79/logs/catalina.out
   echo "Finish..!"
 }
 
